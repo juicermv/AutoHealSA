5970bf7add575dce8ed0213bd45038d4f39c2cc3
juicermv <yonimeller@protonmail.com>
2025-01-20 01:37:51 +0200

    new numake stuff

--- project.lua
+++ project.lua
@@ -1,60 +1,60 @@
 --! NUMAKE PROJECT
-PLUGIN_SDK_DIR = workspace:download_zip("https://github.com/juicermv/plugin-sdk/releases/latest/download/mingw.zip")
+PLUGIN_SDK_DIR = network:zip("https://github.com/juicermv/plugin-sdk/releases/latest/download/mingw.zip")
 
-project = workspace:create_mingw_target("AutoHealSA")
-project.arch = "i686"
-project.output = "AutoHealSA.asi"
+project = Project(
+    "AutoHealSA",
+    "C++",
+    "AutoHealSA.asi",
+    filesystem:walk("AutoHealSA", false, {"cpp"}),
+    {
+        ["README.md"] = "README.md",
+    },
+    {
+        PLUGIN_SDK_DIR .. "/src/",
+        PLUGIN_SDK_DIR .. "/src/plugin_sa/",
+        PLUGIN_SDK_DIR .. "/src/plugin_sa/game_sa/",
+        PLUGIN_SDK_DIR .. "/src/shared/",
+        PLUGIN_SDK_DIR .. "/src/shared/game"
+    },
+    {
+        PLUGIN_SDK_DIR .. "/lib/plugin_sa/",
+        "AutoHealSA"
+    },
+    {
+        "plugin",
+    },
+    {
+        --"_NDEBUG",
+        "_USE_MATH_DEFINES",
+        "_CRT_SECURE_NO_WARNINGS",
+        "_CRT_NON_CONFORMING_SWPRINTFS",
+        "GTASA", 
+        "GTAGAME_NAME=\"San Andreas\"", 
+        "GTAGAME_ABBR=\"SA\"", 
+        "GTAGAME_ABBRLOW=\"sa\"", 
+        "GTAGAME_PROTAGONISTNAME=\"CJ\"", 
+        "GTAGAME_CITYNAME=\"San Andreas\"", 
+        "_LA_SUPPORT", 
+        "_DX9_SDK_INSTALLED", 
+        "PLUGIN_SGV_10US"
+    },
+    {
+        "-std=gnu++23",
+        "-Ofast",
+        "-fpermissive",
+        "-shared",
+        "-flto"
+    },
+    {
+        "--dll",
+        "--subsystem,windows"
+    },
+    nil,
+    nil,
+    "i686",
+    "DynamicLibrary"
+)
 
-project.library_paths = {
-    PLUGIN_SDK_DIR .. "/lib/plugin_sa/",
-    "AutoHealSA"
-}
-
-project.include_paths = {
-    PLUGIN_SDK_DIR .. "/src/",
-    PLUGIN_SDK_DIR .. "/src/plugin_sa/",
-    PLUGIN_SDK_DIR .. "/src/plugin_sa/game_sa/",
-    PLUGIN_SDK_DIR .. "/src/shared/",
-    PLUGIN_SDK_DIR .. "/src/shared/game"
-}
-
-project.definitions = {
-    --"_NDEBUG",
-    "_USE_MATH_DEFINES",
-    "_CRT_SECURE_NO_WARNINGS",
-    "_CRT_NON_CONFORMING_SWPRINTFS",
-    "GTASA", 
-    "GTAGAME_NAME=\"San Andreas\"", 
-    "GTAGAME_ABBR=\"SA\"", 
-    "GTAGAME_ABBRLOW=\"sa\"", 
-    "GTAGAME_PROTAGONISTNAME=\"CJ\"", 
-    "GTAGAME_CITYNAME=\"San Andreas\"", 
-    "_LA_SUPPORT", 
-    "_DX9_SDK_INSTALLED", 
-    "PLUGIN_SGV_10US"
-}
-
-project.libraries = {
-    "plugin",
-}
-
-project.compiler_flags = {
-    "-std=gnu++23",
-    "-Ofast",
-    "-fpermissive",
-    "-shared",
-    "-flto"
-}
-
-project.linker_flags = {
-    "--dll",
-    "--subsystem,windows"
-}
-
-project.assets = {
-    ["README.md"] = "README.md",
-}
-
-project.files = workspace:walk_dir("AutoHealSA", false, {"cpp"})
-
-workspace:register_target(project)
+tasks:create("build_mingw", function()
+    mingw:build(project)
+end)
